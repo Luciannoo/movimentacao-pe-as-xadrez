97ec0dd8c87c076d582dd58e23f0cbb5dcbf684d
Ribeiro <luciano.ribeirodev@gmail.com>
2025-05-17 18:40:14 +0000

    commit da movimentacao das pecas

--- pecas-xadrez.c
+++ pecas-xadrez.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+int main(){
+    //Este desafio foca na movimentação das peças de xadrez usando estruturas de repetição em C.
+    //usando  for, while e do-while para simular movimentos no tabuleiro.
+ 
+    printf("*=+=+=+Movimento da Torre+=+=+=*\n");
+    //movendo a Torre 5 casas para a direita
+    for(int Torre = 0; Torre <= 5; Torre++){
+        printf("Direita\n");
+    }
 
+    printf("\n*=+=+=+Movimento do Bispo+=+=+=*\n\n");
+
+    //Definindo o movimento do Bispo em 5 casas para diagonal
+    int Bispo = 0;
+    while (Bispo <= 5){
+        printf("Cima, Direita\n");
+        Bispo++;
+    }
+
+    printf("\n*=+=+=+Movimento da Rainha+=+=+=*\n\n");
+
+    //definindo o movimento da Rainha para todas as direções 8 casas para esquerda
+    int Rainha = 0;
+    do{
+        printf("Esquerda\n");
+        Rainha++;
+    } while (Rainha <= 8);
+
+}
